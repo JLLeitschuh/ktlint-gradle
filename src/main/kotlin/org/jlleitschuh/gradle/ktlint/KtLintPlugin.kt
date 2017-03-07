b341f08f791f63f0490d988a804777a562b6433a
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2017-03-06 19:00:31 -0500

    Add bulk lint and bulk reformat task

--- src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtLintPlugin.kt
+++ src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtLintPlugin.kt
@@ -13,11 +13,14 @@ import org.gradle.script.lang.kotlin.task
 import org.gradle.script.lang.kotlin.the
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
-
+/**
+ * Task that provides a wrapper over the `ktlint` project.
+ */
 open class KtLintPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         val formattingGroup = "formatting"
 
+        // Only apply this plugin to projects that have the kotlin plugin applied.
         target.pluginManager.withPlugin("kotlin") {
             val ktLintConfig = target.configurations.maybeCreate("ktlint")
 
@@ -25,6 +28,9 @@ open class KtLintPlugin : Plugin<Project> {
                 add(ktLintConfig.name,
                     create(group = "com.github.shyiko", name = "ktlint", version = "0.4.0"))
             }
+
+            val ktlintTask = target.task("ktlint")
+            val ktlintFormatTask = target.task("ktlintFormat")
             target.afterEvaluate {
                 val sourceSets = target.the<JavaPluginConvention>().sourceSets
                 sourceSets.forEach {
@@ -34,7 +40,7 @@ open class KtLintPlugin : Plugin<Project> {
                         .kotlin
                     val sourcePaths = kotlinSourceSet.sourceDirectories.files.map { "${it.path}/**/*.kt" }.toMutableList()
                     // TODO: Add ability to enable debugging and verbose mode
-                    target.task<JavaExec>("ktlint${it.name.capitalize()}") {
+                    val ktlintSourceSetTask = target.task<JavaExec>("ktlint${it.name.capitalize()}") {
                         group = formattingGroup
                         description = "Runs a check against all .kt files to ensure that they are formatted according to ktlint."
                         main = "com.github.shyiko.ktlint.Main"
@@ -42,8 +48,9 @@ open class KtLintPlugin : Plugin<Project> {
                         inputs.dir(kotlinSourceSet)
                         args(sourcePaths)
                     }
+                    ktlintTask.dependsOn(ktlintSourceSetTask)
 
-                    target.task<JavaExec>("ktlint${it.name.capitalize()}Format") {
+                    val ktlintSourceSetFormatTask = target.task<JavaExec>("ktlint${it.name.capitalize()}Format") {
                         group = formattingGroup
                         description = "Runs a check against all .kt files to ensure that they are formatted according to ktlint."
                         main = "com.github.shyiko.ktlint.Main"
@@ -54,6 +61,7 @@ open class KtLintPlugin : Plugin<Project> {
                         sourcePathsWithFormatFlag.add(0, "-F")
                         args(sourcePathsWithFormatFlag)
                     }
+                    ktlintFormatTask.dependsOn(ktlintSourceSetFormatTask)
                 }
             }
         }
