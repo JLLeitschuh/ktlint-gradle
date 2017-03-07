a8f31568d465bddc1b7d0e79d4a3861c26894c5e
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2017-03-07 11:33:54 -0500

    Add publishing to the gradle plugin registry

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,16 +7,19 @@ buildscript {
 
     repositories {
         maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
+        maven { setUrl("https://plugins.gradle.org/m2/") }
         mavenCentral()
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.gradle.publish:plugin-publish-plugin:0.9.7")
     }
 }
 
 apply {
     plugin("kotlin")
     plugin("maven-publish")
+    plugin("com.gradle.plugin-publish")
 }
 group = "org.jlleitschuh.gradle"
 version = "1.0-SNAPSHOT"
@@ -42,3 +45,23 @@ configure<PublishingExtension> {
         }
     }
 }
+
+pluginBundle {
+    vcsUrl = "https://github.com/JLLeitschuh/ktlint-gradle"
+    website = vcsUrl
+    description = "Provides a convenient wrapper plugin over the ktlint project."
+    tags = listOf("ktlint", "kotlin", "linting")
+
+    (plugins) {
+        "ktlintPlugin" {
+            id = "org.jlleitschuh.gradle.ktlint"
+            displayName = "Ktlint Gradle Plugin"
+        }
+    }
+}
+
+/**
+ * Retrieves or configures the [pluginBundle][com.gradle.publish.PluginBundleExtension] project extension.
+ */
+fun Project.pluginBundle(configure: com.gradle.publish.PluginBundleExtension.() -> Unit = {}) =
+    extensions.getByName<com.gradle.publish.PluginBundleExtension>("pluginBundle").apply { configure() }
