854baaf36baee317aa694526fc3b0ed2d297b0dd
Yahor Berdnikau <egorr.berd@gmail.com>
2017-12-31 18:57:15 +0100

    Add Android sample.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 buildscript {
     repositories {
         jcenter()
+        google()
     }
 
     dependencies {
         classpath("org.jlleitschuh.gradle:ktlint-gradle:+")
+        classpath("com.android.tools.build:gradle:3.0.1")
     }
 }
 plugins {
@@ -14,6 +16,7 @@ plugins {
 allprojects {
     repositories {
         jcenter()
+        google()
     }
 }
 
---
+++ samples/android-app/src/androidTest/java/by/egorr/myapplication/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package by.egorr.myapplication
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("by.egorr.myapplication", appContext.packageName)
+    }
+}
