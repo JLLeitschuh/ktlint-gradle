7500590f1c7a15b3e912608ed423fed8085fa950
Yahor Berdnikau <egorr.berd@gmail.com>
2017-12-31 17:40:30 +0100

    Add gradle sample.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- samples/kotlin-ks/src/main/kotlin/org/jlleitschuh/gradle/ktlint/sample/kotlin/main.kt
+++ samples/kotlin-ks/src/main/kotlin/org/jlleitschuh/gradle/ktlint/sample/kotlin/main.kt
@@ -1,5 +1,5 @@
 package org.jlleitschuh.gradle.ktlint.sample.kotlin
 
 fun main(args: Array<String>) {
-    (1..10).map { "Value: $it" }.forEach { println(it) }
+    (1..10).map { "Value: $it" }.forEach { println("Kotlin script: $it") }
 }
