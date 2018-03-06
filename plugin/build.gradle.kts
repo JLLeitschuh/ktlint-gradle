44019f494865763e56a55395e78babdfe337f3ec
Yahor Berdnikau <egorr.berd@gmail.com>
2018-03-06 20:52:15 +0100

    Apply 3.0.1 of plugin to own sources.
    Update version of ktlint plugin used to check it own sources.
    
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "1.2.21"
     id("com.gradle.plugin-publish") version "0.9.7"
     id("maven-publish")
-    id("org.jlleitschuh.gradle.ktlint") version "2.2.1"
+    id("org.jlleitschuh.gradle.ktlint") version "3.0.1"
 }
 
 group = "org.jlleitschuh.gradle"
