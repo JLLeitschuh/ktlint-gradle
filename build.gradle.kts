c8c0ead1ae602928f744fdd317d1542ea91cd70d
uzzu <himeatball@gmail.com>
2018-04-25 18:47:04 +0900

    Add ktlint task dependencies in root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,3 +26,9 @@ task<Wrapper>("wrapper") {
     gradleVersion = SamplesVersions.gradleWrapper
     distributionType = Wrapper.DistributionType.ALL
 }
+
+tasks {
+    "ktlintCheck" {
+        dependsOn(":samples:kotlin-rulesets-creating:build")
+    }
+}
