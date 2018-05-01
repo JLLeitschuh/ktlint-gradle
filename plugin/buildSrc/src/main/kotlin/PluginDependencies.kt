bb33efd13f6f42269c0aa97545ae2116d55ea883
Yahor Berdnikau <egorr.berd@gmail.com>
2018-05-01 17:23:30 +0200

    Bump kotlin native to 0.7 version.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -3,7 +3,7 @@ object PluginVersions {
     val ktlintPlugin = "3.3.0"
     val gradlePublishPlugin = "0.9.10"
     val androidPlugin = "3.1.1"
-    val kotlinNativePlugin = "0.6.1"
+    val kotlinNativePlugin = "0.7"
     val semver = "1.0.0"
     val gradleWrapper = "4.6"
 }
