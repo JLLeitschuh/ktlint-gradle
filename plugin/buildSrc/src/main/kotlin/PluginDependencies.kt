83906c555e1f93cc3d556f77ccc96e593094a91d
Yahor Berdnikau <egorr.berd@gmail.com>
2018-06-26 20:08:54 +0200

    Update plugin itself to 4.1.0 version.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -1,6 +1,6 @@
 object PluginVersions {
     val kotlin = "1.2.50"
-    val ktlintPlugin = "4.0.0"
+    val ktlintPlugin = "4.1.0"
     val gradlePublishPlugin = "0.9.10"
     val androidPlugin = "3.1.2"
     val kotlinNativePlugin = "0.7"
