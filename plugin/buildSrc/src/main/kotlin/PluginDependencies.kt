a22c5c2b38b2d60cb51f9b973cca8ec75a1827d1
Yahor Berdnikau <egorr.berd@gmail.com>
2018-06-26 20:11:32 +0200

    Update kotlin native to 0.7.1 version.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -3,7 +3,7 @@ object PluginVersions {
     val ktlintPlugin = "4.1.0"
     val gradlePublishPlugin = "0.9.10"
     val androidPlugin = "3.1.2"
-    val kotlinNativePlugin = "0.7"
+    val kotlinNativePlugin = "0.7.1"
     val semver = "1.0.0"
     val gradleWrapper = "4.7"
     val junit = "4.12"
