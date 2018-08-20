33e19001bbd0160c054f9775e91b0aa7a0ddb3f3
Yahor Berdnikau <egorr.berd@gmail.com>
2018-08-20 21:34:48 +0200

    Update android plugin to 3.1.4 version.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -2,7 +2,7 @@ object PluginVersions {
     val kotlin = "1.2.61"
     val ktlintPlugin = "5.0.0"
     val gradlePublishPlugin = "0.9.10"
-    val androidPlugin = "3.1.2"
+    val androidPlugin = "3.1.4"
     val kotlinNativePlugin = "0.7.1"
     val semver = "1.0.0"
     val gradleWrapper = "4.8.1"
