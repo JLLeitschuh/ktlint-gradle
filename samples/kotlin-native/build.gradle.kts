9490f3f81b67eebe051e96a4f78a2969d22e9951
Yahor Berdnikau <egorr.berd@gmail.com>
2018-09-03 21:18:18 +0200

    Improve configuration for new native sample.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- samples/kotlin-native/build.gradle.kts
+++ samples/kotlin-native/build.gradle.kts
@@ -1,8 +1,19 @@
+import org.jetbrains.kotlin.gradle.plugin.experimental.internal.KotlinNativeMainComponent
+import org.jetbrains.kotlin.gradle.plugin.experimental.internal.OutputKind
+import org.jetbrains.kotlin.gradle.plugin.experimental.plugins.kotlinNativeSourceSets
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 plugins.apply("org.jlleitschuh.gradle.ktlint")
 apply {
     plugin("org.jetbrains.kotlin.native")
+}
+
+kotlinNativeSourceSets["main"].component {
+    // https://github.com/JetBrains/kotlin-native/issues/1807
+    if (this is KotlinNativeMainComponent) {
+        outputKinds.set(listOf(OutputKind.EXECUTABLE))
+        baseName.set("foo")
+    }
 }
 
 configure<KtlintExtension> {
