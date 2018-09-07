16489fc9146738896a8376e7b7d11125ee53a85c
Yahor Berdnikau <egorr.berd@gmail.com>
2018-09-07 23:01:27 +0200

    Remove afterEvaluate in idea plugin.

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintIdeaPlugin.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintIdeaPlugin.kt
@@ -20,30 +20,27 @@ open class KtlintIdeaPlugin : Plugin<Project> {
     }
 
     private fun addApplyToIdeaTasks(rootProject: Project, extension: KtlintExtension) {
-        rootProject.afterEvaluate {
-            if (rootProject.tasks.findByName(APPLY_TO_IDEA_TASK_NAME) == null) {
-                val ktLintConfig = createConfiguration(rootProject, extension)
+        rootProject.logger.warn("Creating idea helper tasks")
+        val ktLintConfig = createConfiguration(rootProject, extension)
 
-                if (extension.isApplyToIdeaPerProjectAvailable()) {
-                    rootProject.taskHelper<KtlintApplyToIdeaTask>(APPLY_TO_IDEA_TASK_NAME) {
-                        group = HELP_GROUP
-                        description = "Generates IDEA built-in formatter rules and apply them to the project." +
-                            "It will overwrite existing ones."
-                        classpath.setFrom(ktLintConfig)
-                        android.set(rootProject.provider { extension.isAndroidFlagEnabled() })
-                        globally.set(rootProject.provider { false })
-                    }
-                }
-
-                rootProject.taskHelper<KtlintApplyToIdeaTask>(APPLY_TO_IDEA_GLOBALLY_TASK_NAME) {
-                    group = HELP_GROUP
-                    description = "Generates IDEA built-in formatter rules and apply them globally " +
-                        "(in IDEA user settings folder). It will overwrite existing ones."
-                    classpath.setFrom(ktLintConfig)
-                    android.set(rootProject.provider { extension.isAndroidFlagEnabled() })
-                    globally.set(rootProject.provider { true })
-                }
+        if (extension.isApplyToIdeaPerProjectAvailable()) {
+            rootProject.maybeCreateTask<KtlintApplyToIdeaTask>(APPLY_TO_IDEA_TASK_NAME) {
+                group = HELP_GROUP
+                description = "Generates IDEA built-in formatter rules and apply them to the project." +
+                    "It will overwrite existing ones."
+                classpath.setFrom(ktLintConfig)
+                android.set(rootProject.provider { extension.isAndroidFlagEnabled() })
+                globally.set(rootProject.provider { false })
             }
+        }
+
+        rootProject.maybeCreateTask<KtlintApplyToIdeaTask>(APPLY_TO_IDEA_GLOBALLY_TASK_NAME) {
+            group = HELP_GROUP
+            description = "Generates IDEA built-in formatter rules and apply them globally " +
+                "(in IDEA user settings folder). It will overwrite existing ones."
+            classpath.setFrom(ktLintConfig)
+            android.set(rootProject.provider { extension.isAndroidFlagEnabled() })
+            globally.set(rootProject.provider { true })
         }
     }
 
