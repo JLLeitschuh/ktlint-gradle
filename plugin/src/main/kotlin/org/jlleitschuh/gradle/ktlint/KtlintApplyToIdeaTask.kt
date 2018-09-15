094995594f19bf25a2f4d94797afb5f72a09b317
Yahor Berdnikau <egorr.berd@gmail.com>
2018-09-15 22:06:39 +0200

    Change exception type to GradleException.
    Throwing this exception will fail the build, restoring old behaviour.

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintApplyToIdeaTask.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintApplyToIdeaTask.kt
@@ -2,12 +2,12 @@ package org.jlleitschuh.gradle.ktlint
 
 import net.swiftzer.semver.SemVer
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.StopExecutionException
 import org.gradle.api.tasks.TaskAction
 import javax.inject.Inject
 
@@ -31,7 +31,7 @@ open class KtlintApplyToIdeaTask @Inject constructor(
     fun generate() {
         if (!globally.get() && !isApplyToIdeaPerProjectAvailable()) {
             logger.error("Apply per project in only available from ktlint 0.22.0")
-            throw StopExecutionException("Apply per project in only available from ktlint 0.22.0")
+            throw GradleException("Apply per project in only available from ktlint 0.22.0")
         }
 
         project.javaexec {
