2553a55292271fb43fa84a58f4b5e3abe0bb611d
Yahor Berdnikau <egorr.berd@gmail.com>
2018-10-06 08:49:47 +0200

    Update SemVer to 1.1.0 version.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -4,7 +4,7 @@ object PluginVersions {
     val gradlePublishPlugin = "0.9.10"
     val androidPlugin = "3.2.0"
     val kotlinNativePlugin = "0.8.2"
-    val semver = "1.0.0"
+    val semver = "1.1.0"
     val gradleWrapper = "4.10.2"
     val junit = "4.12"
 }
