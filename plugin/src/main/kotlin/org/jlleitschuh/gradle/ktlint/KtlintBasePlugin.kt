1608733b2d9570754e642d64354ebfcd121ce284
Yahor Berdnikau <egorr.berd@gmail.com>
2018-10-17 19:56:37 +0200

    Added typealias for filter applier function.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintBasePlugin.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintBasePlugin.kt
@@ -6,6 +6,8 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.util.PatternFilterable
 import org.gradle.util.GradleVersion
 
+typealias FilterApplier = (Action<PatternFilterable>) -> Unit
+
 /**
  * The base Ktlint plugin that all other plugins are built on.
  */
@@ -13,14 +15,15 @@ open class KtlintBasePlugin : Plugin<Project> {
     internal lateinit var extension: KtlintExtension
 
     override fun apply(target: Project) {
-        val filterTargetApplier = fun(action: Action<PatternFilterable>) {
+        val filterTargetApplier: FilterApplier = {
             if (GradleVersion.current() >= GradleVersion.version("4.9")) {
                 // API only added in Gradle 4.9
-                target.tasks.withType(KtlintCheckTask::class.java).configureEach(action)
+                target.tasks.withType(KtlintCheckTask::class.java).configureEach(it)
             } else {
-                target.tasks.withType(KtlintCheckTask::class.java, action)
+                target.tasks.withType(KtlintCheckTask::class.java, it)
             }
         }
+
         extension = target.extensions.create(
             "ktlint",
             KtlintExtension::class.java,
