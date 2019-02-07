edde60271aa44b2874e586da3eda087b5cf50ef8
Yahor Berdnikau <egorr.berd@gmail.com>
2019-02-07 22:55:02 +0100

    Update Kotlin to 1.3.21 version.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -1,5 +1,5 @@
 object PluginVersions {
-    val kotlin = "1.3.10"
+    val kotlin = "1.3.21"
     val ktlintPlugin = "6.3.1"
     val gradlePublishPlugin = "0.10.0"
     val androidPlugin = "3.2.0"
