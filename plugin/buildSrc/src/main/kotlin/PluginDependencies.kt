dd961b17c2ef7568f194fb4380ef30aea03f9f21
Yahor Berdnikau <egorr.berd@gmail.com>
2019-03-31 14:37:37 +0200

    Update Gradle Publish plugin to 0.10.1 version.

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -1,7 +1,7 @@
 object PluginVersions {
     val kotlin = "1.3.21"
     val ktlintPlugin = "7.2.0"
-    val gradlePublishPlugin = "0.10.0"
+    val gradlePublishPlugin = "0.10.1"
     val androidPlugin = "3.3.0"
     val semver = "1.1.1"
     val gradleWrapper = "4.10.2"
