0b1ae10f327f866789509a5a1031dd68e7b45e41
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-04-12 13:37:46 -0500

    Upgrade to kotlin 1.3.30

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -1,5 +1,5 @@
 object PluginVersions {
-    val kotlin = "1.3.21"
+    val kotlin = "1.3.30"
     val ktlintPlugin = "7.2.0"
     val gradlePublishPlugin = "0.10.1"
     val androidPlugin = "3.3.0"
