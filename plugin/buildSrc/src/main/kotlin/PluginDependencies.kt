86655f1a3c8ab30bde4a200d958b894bdd744046
Yahor Berdnikau <egorr.berd@gmail.com>
2019-05-15 20:36:52 +0200

    Apply latest version of plugin on itself.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -1,6 +1,6 @@
 object PluginVersions {
     val kotlin = "1.3.31"
-    val ktlintPlugin = "7.4.0"
+    val ktlintPlugin = "8.0.0"
     val gradlePublishPlugin = "0.10.1"
     val androidPlugin = "3.4.0"
     val semver = "1.1.1"
