142a54abcd7134155f08767ff7d44305ef67ff9e
Yahor Berdnikau <egorr.berd@gmail.com>
2019-09-15 13:35:20 +0200

    Update Android Gradle plugin to 3.5.0 version.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -2,7 +2,7 @@ object PluginVersions {
     val kotlin = "1.3.50"
     val ktlintPlugin = "8.2.0"
     val gradlePublishPlugin = "0.10.1"
-    val androidPlugin = "3.4.0"
+    val androidPlugin = "3.5.0"
     val semver = "1.1.1"
     val gradleWrapper = "5.6"
     val junit5 = "5.4.0"
