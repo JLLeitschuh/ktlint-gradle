ab252b8cd168c4094946bfeaacaf67407bd5c231
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-06-03 17:52:13 -0700

    Update Android Gradle plugin to 3.6.3

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -1,8 +1,8 @@
 object PluginVersions {
-    val kotlin = "1.3.60"
+    val kotlin = "1.3.72"
     val ktlintPlugin = "9.1.1"
     val gradlePublishPlugin = "0.10.1"
-    val androidPlugin = "3.6.0"
+    val androidPlugin = "3.6.3"
     val semver = "1.1.1"
     val jgit = "5.6.0.201912101111-r"
     val gradleWrapper = "6.2.1"
