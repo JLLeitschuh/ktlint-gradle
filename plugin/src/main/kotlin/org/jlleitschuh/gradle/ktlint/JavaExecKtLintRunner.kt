bbaffd2ef4dd119c76ff59cd63c1dcfdc9d71cb6
Sterling Greene <sterling.greene@gmail.com>
2020-07-08 17:02:23 -0400

    Make JavaExecKtLintRunner creatable by ObjectFactory

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/JavaExecKtLintRunner.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/JavaExecKtLintRunner.kt
@@ -1,10 +1,11 @@
 package org.jlleitschuh.gradle.ktlint
 
 import java.io.File
+import javax.inject.Inject
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 
-class JavaExecKtLintRunner(private val project: Project) : KtLintRunner {
+open class JavaExecKtLintRunner @Inject constructor(private val project: Project) : KtLintRunner {
     override fun lint(ktlintClasspath: FileCollection, ktlintVersion: String, ignoreFailures: Boolean, ktlintArgsFile: File) {
         project.javaexec {
             it.classpath = ktlintClasspath
