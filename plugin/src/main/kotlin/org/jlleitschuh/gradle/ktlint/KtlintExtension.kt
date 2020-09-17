b7e5c2d335daab8921075c34659132a07120a391
Zhenlei Ji <zhenlei.ji@gmail.com>
2020-09-18 00:21:38 +0200

    Update ktlint to 0.39.0

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintExtension.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/KtlintExtension.kt
@@ -33,7 +33,7 @@ internal constructor(
     /**
      * The version of ktlint to use.
      */
-    val version: Property<String> = objectFactory.property { set("0.38.1") }
+    val version: Property<String> = objectFactory.property { set("0.39.0") }
 
     /**
      * Enable verbose mode.
