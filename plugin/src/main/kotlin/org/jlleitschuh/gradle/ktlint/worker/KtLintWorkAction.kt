db347ae9ec179add14a661f782c5dde482341122
Yahor Berdnikau <egorr.berd@gmail.com>
2021-01-01 21:02:16 +0100

    Update KtLintWorkAction to use ktlint-core package.
    Now it directly runs KtLint rather then invoking it via CLI.

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/worker/KtLintWorkAction.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/worker/KtLintWorkAction.kt
@@ -1,33 +1,91 @@
 package org.jlleitschuh.gradle.ktlint.worker
 
+import com.pinterest.ktlint.core.KtLint
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
-import org.gradle.process.ExecOperations
+import org.gradle.api.provider.SetProperty
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
-import org.jlleitschuh.gradle.ktlint.resolveMainClassName
-import javax.inject.Inject
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
 @Suppress("UnstableApiUsage")
-abstract class KtLintWorkAction : WorkAction<KtLintParameters> {
-    @get:Inject
-    abstract val execOperations: ExecOperations
-
+abstract class KtLintWorkAction : WorkAction<KtLintWorkAction.KtLintWorkParameters> {
     override fun execute() {
-        execOperations.javaexec {
-            it.classpath = parameters.ktlintClasspath
-            it.main = resolveMainClassName(parameters.ktlintVersion.get())
-            it.isIgnoreExitValue = parameters.ignoreFailures.get()
-            it.args("@${parameters.ktlintArgsFile.asFile.get()}")
+        val serializedRuleSets = parameters.loadedRuleSets.asFile.get()
+        val ruleSets = ObjectInputStream(FileInputStream(serializedRuleSets))
+            .use {
+                @Suppress("UNCHECKED_CAST")
+                it.readObject() as List<SerializableRuleSet>
+            }
+            .map { it.ruleSet }
+
+        val additionalEditorConfig = parameters
+            .additionalEditorconfigFile
+            .orNull
+            ?.asFile
+            ?.absolutePath
+        val userData = generateUserData()
+        val debug = parameters.debug.get()
+        val formatSource = parameters.formatSource.getOrElse(false)
+
+        val errors = mutableMapOf<SerializableLintError, Boolean>()
+
+        parameters.filesToLint.files.forEach {
+            val ktlintParameters = KtLint.Params(
+                fileName = it.absolutePath,
+                text = it.readText(),
+                ruleSets = ruleSets,
+                userData = userData,
+                debug = debug,
+                editorConfigPath = additionalEditorConfig,
+                script = !it.name.endsWith(".kt", ignoreCase = true),
+                cb = { lintError, isCorrected ->
+                    errors[SerializableLintError(lintError)] = isCorrected
+                }
+            )
+
+            if (formatSource) {
+                KtLint.format(ktlintParameters)
+            } else {
+                KtLint.lint(ktlintParameters)
+            }
+        }
+
+        ObjectOutputStream(
+            FileOutputStream(
+                parameters.discoveredErrorsFile.asFile.get()
+            )
+        ).use {
+            it.writeObject(errors)
         }
     }
-}
 
-@Suppress("UnstableApiUsage")
-interface KtLintParameters : WorkParameters {
-    val ktlintClasspath: ConfigurableFileCollection
-    val ktlintVersion: Property<String>
-    val ignoreFailures: Property<Boolean>
-    val ktlintArgsFile: RegularFileProperty
+    private fun generateUserData(): Map<String, String> {
+        val userData = mutableMapOf(
+            "android" to parameters.android.get().toString()
+        )
+        val disabledRules = parameters.disabledRules.get()
+        if (disabledRules.isNotEmpty()) {
+            userData["disabled_rules"] = disabledRules.joinToString(
+                separator = ","
+            )
+        }
+
+        return userData.toMap()
+    }
+
+    interface KtLintWorkParameters : WorkParameters {
+        val loadedRuleSets: RegularFileProperty
+        val filesToLint: ConfigurableFileCollection
+        val android: Property<Boolean>
+        val disabledRules: SetProperty<String>
+        val debug: Property<Boolean>
+        val additionalEditorconfigFile: RegularFileProperty
+        val formatSource: Property<Boolean>
+        val discoveredErrorsFile: RegularFileProperty
+    }
 }
