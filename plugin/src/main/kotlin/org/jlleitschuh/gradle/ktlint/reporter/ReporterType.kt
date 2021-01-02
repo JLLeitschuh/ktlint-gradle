03d045597ff242155a9eb38dab05ee06a9390417
Yahor Berdnikau <egorr.berd@gmail.com>
2021-01-02 12:33:18 +0100

    Make ReporterType serializable.

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/reporter/ReporterType.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/reporter/ReporterType.kt
@@ -1,15 +1,20 @@
 package org.jlleitschuh.gradle.ktlint.reporter
 
 import net.swiftzer.semver.SemVer
+import java.io.Serializable
 
 enum class ReporterType(
     val reporterName: String,
     val availableSinceVersion: SemVer,
     val fileExtension: String
-) {
+) : Serializable {
     PLAIN("plain", SemVer(0, 9, 0), "txt"),
     PLAIN_GROUP_BY_FILE("plain?group_by_file", SemVer(0, 9, 0), "txt"),
     CHECKSTYLE("checkstyle", SemVer(0, 9, 0), "xml"),
     JSON("json", SemVer(0, 9, 0), "json"),
     HTML("html", SemVer(0, 36, 0), "html");
+
+    companion object {
+        private const val serialVersionUID: Long = 201202199L
+    }
 }
