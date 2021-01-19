3ce901a5d77422b73fb044944097816382e27b6b
Yahor Berdnikau <egorr.berd@gmail.com>
2021-01-19 21:46:03 +0100

    Add docs to LintErrorResult class.

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/worker/LintErrorResult.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/worker/LintErrorResult.kt
@@ -3,6 +3,12 @@ package org.jlleitschuh.gradle.ktlint.worker
 import java.io.File
 import java.io.Serializable
 
+/**
+ * Represents result of file code style check.
+ *
+ * @param lintedFile file that was checked by KtLint
+ * @param lintErrors list of found errors and flag indicating if this error was corrected
+ */
 internal data class LintErrorResult(
     val lintedFile: File,
     val lintErrors: List<Pair<SerializableLintError, Boolean>>
