1efbca06db70ac6c9749eeebaa20508f5728f45b
Goooler <wangzongler@gmail.com>
2021-04-09 23:43:21 +0800

    Update dependencies

--- plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
+++ plugin/buildSrc/src/main/kotlin/PluginDependencies.kt
@@ -1,9 +1,9 @@
 object PluginVersions {
-    val kotlin = "1.4.30"
-    val ktlintPlugin = "9.3.0"
+    val kotlin = "1.4.32"
+    val ktlintPlugin = "10.0.0"
     val ktlint = "0.41.0"
-    val gradlePublishPlugin = "0.12.0"
-    val androidPlugin = "4.1.0"
+    val gradlePublishPlugin = "0.14.0"
+    val androidPlugin = "4.1.3"
     val semver = "1.1.1"
     val jgit = "5.6.0.201912101111-r"
     val sl4f = "1.7.30"
