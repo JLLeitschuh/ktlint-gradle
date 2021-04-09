6bd148c4bcb0f65aec680b9d832b0b7ae3261b9e
Goooler <wangzongler@gmail.com>
2021-04-09 23:55:09 +0800

    Update dependencies

--- buildSrc/src/main/kotlin/SamplesDependencies.kt
+++ buildSrc/src/main/kotlin/SamplesDependencies.kt
@@ -4,12 +4,12 @@ object SamplesVersions {
     val gradleWrapper = PluginVersions.gradleWrapper
     val gradleWrapperSha = PluginVersions.gradleWrapperSha
     val ktlintCore = "0.32.0"
-    val junit = "4.12"
+    val junit = "4.13.2"
 
     // Android sample dependencies
-    val androidXFragment = "1.2.5"
-    val androidXRecyclerView = "1.1.0"
-    val androidMaterial = "1.2.1"
+    val androidXFragment = "1.3.2"
+    val androidXRecyclerView = "1.2.0"
+    val androidMaterial = "1.3.0"
     val androidXTestRunner = "1.3.0"
     val androidXEspresso = "3.3.0"
 }
