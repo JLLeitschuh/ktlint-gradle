2100fa167a29d8e01527c9e306c984f4192d48e7
Goooler <wangzongler@gmail.com>
2021-04-10 00:05:37 +0800

    Mak versions const

--- buildSrc/src/main/kotlin/SamplesDependencies.kt
+++ buildSrc/src/main/kotlin/SamplesDependencies.kt
@@ -1,15 +1,15 @@
 object SamplesVersions {
-    val kotlin = PluginVersions.kotlin
-    val androidPlugin = PluginVersions.androidPlugin
-    val gradleWrapper = PluginVersions.gradleWrapper
-    val gradleWrapperSha = PluginVersions.gradleWrapperSha
-    val ktlintCore = "0.32.0"
-    val junit = "4.13.2"
+    const val kotlin = PluginVersions.kotlin
+    const val androidPlugin = PluginVersions.androidPlugin
+    const val gradleWrapper = PluginVersions.gradleWrapper
+    const val gradleWrapperSha = PluginVersions.gradleWrapperSha
+    const val ktlintCore = "0.32.0"
+    const val junit = "4.13.2"
 
     // Android sample dependencies
-    val androidXFragment = "1.3.2"
-    val androidXRecyclerView = "1.2.0"
-    val androidMaterial = "1.3.0"
-    val androidXTestRunner = "1.3.0"
-    val androidXEspresso = "3.3.0"
+    const val androidXFragment = "1.3.2"
+    const val androidXRecyclerView = "1.2.0"
+    const val androidMaterial = "1.3.0"
+    const val androidXTestRunner = "1.3.0"
+    const val androidXEspresso = "3.3.0"
 }
