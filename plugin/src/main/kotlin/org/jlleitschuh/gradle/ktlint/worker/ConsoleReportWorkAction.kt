cf9748113ab7d3ad3e0a2ea5ce2326c926899d3f
Vanessa Johnson <vmjohnson999@gmail.com>
2025-08-19 01:02:33 -0500

    added integration test, made edits to reporter and consoleReportWorkAction specified in comments

--- plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/worker/ConsoleReportWorkAction.kt
+++ plugin/src/main/kotlin/org/jlleitschuh/gradle/ktlint/worker/ConsoleReportWorkAction.kt
@@ -6,9 +6,7 @@ import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.logging.Logging
-import org.gradle.api.problems.Problems
 import org.gradle.api.provider.Property
-import org.gradle.util.GradleVersion
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import org.jetbrains.kotlin.util.prefixIfNot
@@ -18,10 +16,10 @@ import javax.inject.Inject
 
 internal abstract class ConsoleReportWorkAction : WorkAction<ConsoleReportWorkAction.ConsoleReportParameters> {
 
-    @get:Inject
-    abstract val problems: Problems
-
     private val logger = Logging.getLogger("ktlint-console-report-worker")
+
+    @Inject
+    private lateinit var problemsApiReporter: ProblemsApiReporter
 
     override fun execute() {
         val errors = KtLintClassesSerializer
@@ -49,15 +47,17 @@ internal abstract class ConsoleReportWorkAction : WorkAction<ConsoleReportWorkAc
         }
 
         val isLintErrorsFound = lintErrors.values.flatten().isNotEmpty()
-        // The gradle version needs to be at least 8.14 to use the problems API
-        if (isLintErrorsFound && GradleVersion.current() >= GradleVersion.version("8.14")) {
+
+        // Report problems to Gradle Problems API if available
+        if (isLintErrorsFound) {
             try {
-                val problemsApiReporter = ProblemsApiReporter(problems)
-                problemsApiReporter.reportProblems(lintErrors)
+                problemsApiReporter.reportProblems(lintErrors, parameters.ignoreFailures.getOrElse(false))
             } catch (e: Exception) {
-                logger.error("Failed to report problems using Problems API.", e)
+                // Problems API might not be available in all Gradle versions
+                logger.debug("Problems API not available: ${e.message}")
             }
         }
+
         if (parameters.outputToConsole.getOrElse(false) && isLintErrorsFound) {
             val verbose = parameters.verbose.get()
             lintErrors.forEach { (filePath, errors) ->
